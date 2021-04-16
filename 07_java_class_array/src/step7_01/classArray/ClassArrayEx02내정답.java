b3676a792958103462b962b605bfd3e3cbda9f05
user <user@401-10>
2021-04-16 17:44:58 +0900

    04/16 17:44 학원

---
+++ 07_java_class_array/src/step7_01/classArray/ClassArrayEx02내정답.java
@@ -0,0 +1,33 @@
+package step7_01.classArray;
+//2021/04/16 17:12 ~ 17:21
+class Card_1{
+	
+	int num;
+	String shape;
+	
+}
+
+public class ClassArrayEx02내정답 {
+
+	public static void main(String[] args) {
+		
+		 Card_1[] arr = new Card_1[10]; // 클래스 배열 생성
+		 
+		 
+		 
+		 // for문 활용
+		 for (int i = 0; i < 10;  i++) {
+			 arr[i] = new Card_1();
+			 arr[i].num = (i + 1);
+			 arr[i].shape = "클로버";
+		 }
+
+		 for (int i = 0; i < arr.length; i++) {
+			System.out.println("num : " + arr[i].num); //1,2,3,4,5,6,7,8,9,10
+			System.out.println("shape : " + arr[i].shape); // 클로버
+			System.out.println();
+		}
+		 
+	}
+
+}
---
+++ 07_java_class_array/src/step7_01/classArray/ClassArrayEx03내정답.java
@@ -0,0 +1,40 @@
+package step7_01.classArray;
+//2021/04/16 17:35 ~ 
+
+class Client_1{
+	
+	String name;
+	int score;
+	
+	void setData (String name, int score) {  
+		this.name = name ; 		// 멤버변수에 파라메타로 넘어온 데이터를 대입
+		this.score = score;		// 멤버변수에 파라메타로 넘어온 데이터를 대입
+	}
+	
+	void printData() {
+		System.out.println(this.name  + " : " + this.score);
+	}
+	
+}
+
+public class ClassArrayEx03내정답 {
+
+	public static void main(String[] args) {
+		
+		Client_1[] clients = new Client_1[3];
+		
+		for (int i = 0; i < clients.length; i++) {
+			clients[i] = new Client_1();
+		}
+		
+		clients[0].setData("김철수",100); 
+		clients[1].setData("이만수",20);
+		clients[2].setData("박영희",70);
+		
+		for (int i = 0 ; i < clients.length; i++) {
+			clients[i].printData();
+		}
+
+	}
+
+}
